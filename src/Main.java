c7af80962027fb2d3767ac7bbdf05d2e3f69c3dc
Jelle Schut <jelleschut@hotmail.com>
2019-09-30 14:27:37 +0200

    wat een onzin

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String... args) {
-        System.out.println("Hello anderebranch");
+        System.out.println("Hello anderebranch2");
         System.out.println("Helle Git2");
 
         AndereBranch andereBranch = new AndereBranch();

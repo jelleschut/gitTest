961f4e9d8474a24ab73d5989c6fa735f0922c2b8
Jelle Schut <jelleschut@hotmail.com>
2019-09-30 14:24:14 +0200

    merge conflict gemaakt

---
+++ src/AndereBranch.java
@@ -0,0 +1,5 @@
+public class AndereBranch {
+    AndereBranch() {
+        System.out.println("Andere branch");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String... args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello anderebranch");
         System.out.println("Helle Git2");
+
+        AndereBranch andereBranch = new AndereBranch();
     }
 }

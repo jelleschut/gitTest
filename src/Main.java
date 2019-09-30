307b911a40f0447feba91cbe2047fb3d19d3cb79
Jelle Schut <jelleschut@hotmail.com>
2019-09-30 15:06:19 +0200

    ssh test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String... args) {
         System.out.println("Hello anderebranch2");
         System.out.println("Helle Git2");
+        System.out.println("sshtest");
 
         AndereBranch andereBranch = new AndereBranch();
     }
